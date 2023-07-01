3b047117c14dbf25628ea8e653709c90af8b32c2
Titan Elysion <65347415+TitanElysion@users.noreply.github.com>
2023-07-01 18:01:24 +0100

    Add files via upload
    v0.1.1

--- script/script.js
+++ script/script.js
@@ -1,21 +1,8 @@
-/* // Afficher ou masquer le bouton "Retour en haut" en fonction de la position de la page
-window.onscroll = function() {
-    scrollFunction();
-  };
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("back-to-top").style.display = "block";
-    } else {
-      document.getElementById("back-to-top").style.display = "none";
-    }
-  }
-  
-  // Faire défiler la page jusqu'en haut lorsque le bouton "Retour en haut" est cliqué
-  document.getElementById("back-to-top").onclick = function() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  };
-   */
+// Ajoute/retire la classe "active" lors du clic sur l'icône du menu hamburger
+var navToggle = document.querySelector('.nav-toggle');
+var navMenu = document.querySelector('nav ul');
 
-  
+navToggle.addEventListener('click', function() {
+    this.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
